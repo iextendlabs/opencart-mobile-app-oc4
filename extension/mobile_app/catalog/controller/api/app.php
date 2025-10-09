81843d2b8a107e547f94bcfd6797c69ffbbb1c1e
Saqib Ashraf <linktosaqib@gmail.com>
2025-10-09 21:28:09 +0500

    logout api

--- extension/mobile_app/catalog/controller/api/app.php
+++ extension/mobile_app/catalog/controller/api/app.php
@@ -841,4 +841,30 @@ class App extends \Opencart\System\Engine\Controller
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($json));
     }
+
+    public function logout(): void {
+        // Initialize the response array
+        $json = [];
+
+        // Clear customer session
+        $this->customer->logout();
+        unset($this->session->data['shipping_address']);
+        unset($this->session->data['shipping_method']);
+        unset($this->session->data['shipping_methods']);
+        unset($this->session->data['payment_address']);
+        unset($this->session->data['payment_method']);
+        unset($this->session->data['payment_methods']);
+        unset($this->session->data['comment']);
+        unset($this->session->data['order_id']);
+        unset($this->session->data['coupon']);
+        unset($this->session->data['reward']);
+        unset($this->session->data['voucher']);
+        unset($this->session->data['vouchers']);
+
+        $json['success'] = true;
+        $json['message'] = 'You have been successfully logged out';
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
 }
