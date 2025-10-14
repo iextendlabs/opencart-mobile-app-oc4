5da9d0707df2ff08ffa4036e670dce5cccd0f53c
Saqib Ashraf <linktosaqib@gmail.com>
2025-10-14 14:36:07 +0500

    currency api

--- extension/mobile_app/catalog/controller/api/app.php
+++ extension/mobile_app/catalog/controller/api/app.php
@@ -11,6 +11,36 @@ namespace Opencart\Catalog\Controller\Extension\MobileApp\Api;
 
 class App extends \Opencart\System\Engine\Controller
 {
+    public function __construct($registry) {
+        parent::__construct($registry);
+        $this->initCurrency();
+    }
+
+    private function initCurrency(): void {
+        $this->load->model('localisation/currency');
+        
+        // Check currency from header first
+        $currency_code = isset($this->request->server['HTTP_X_CURRENCY']) ? $this->request->server['HTTP_X_CURRENCY'] : '';
+        
+        // If no header, check from request parameter
+        if (!$currency_code && isset($this->request->get['currency'])) {
+            $currency_code = $this->request->get['currency'];
+        }
+        
+        // If currency code is provided, validate it
+        if ($currency_code) {
+            $currency_info = $this->model_localisation_currency->getCurrencyByCode($currency_code);
+            if ($currency_info && $currency_info['status']) {
+                $this->session->data['currency'] = $currency_code;
+                return;
+            }
+        }
+        
+        // If no valid currency is set, use default
+        if (empty($this->session->data['currency'])) {
+            $this->session->data['currency'] = $this->config->get('config_currency');
+        }
+    }
 
     public function getHomePageData(): void
     {
@@ -1358,6 +1388,87 @@ class App extends \Opencart\System\Engine\Controller
             }
         }
 
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    public function getCurrencies(): void {
+        $this->load->language('common/currency');
+        $this->load->model('localisation/currency');
+        
+        $json = [];
+        $json['currencies'] = [];
+        
+        $results = $this->model_localisation_currency->getCurrencies();
+        
+        foreach ($results as $result) {
+            if ($result['status']) {
+                $json['currencies'][$result['code']] = [
+                    'title'        => $result['title'],
+                    'code'         => $result['code'],
+                    'symbol_left'  => $result['symbol_left'],
+                    'symbol_right' => $result['symbol_right']
+                ];
+            }
+        }
+        
+        $code = $this->session->data['currency'];
+        
+        $json['code'] = $code;
+        $json['active_currency'] = [
+            'title'        => $results[$code]['title'],
+            'code'         => $code,
+            'symbol_left'  => $results[$code]['symbol_left'],
+            'symbol_right' => $results[$code]['symbol_right']
+        ];
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    public function setCurrency(): void {
+        $this->load->language('common/currency');
+        
+        $json = [];
+        
+        if (!isset($this->request->post['code'])) {
+            $json['error'] = $this->language->get('error_currency');
+        }
+        
+        $this->load->model('localisation/currency');
+        
+        $currency_info = $this->model_localisation_currency->getCurrencyByCode($this->request->post['code']);
+        
+        if (!$currency_info) {
+            $json['error'] = $this->language->get('error_currency');
+        }
+        
+        if (!$json) {
+            $this->session->data['currency'] = $this->request->post['code'];
+            
+            // Clear shipping methods when currency is changed
+            unset($this->session->data['shipping_method']);
+            unset($this->session->data['shipping_methods']);
+            
+            // Set currency cookie
+            $option = [
+                'expires'  => time() + 60 * 60 * 24 * 30,
+                'path'     => '/',
+                'SameSite' => 'Lax'
+            ];
+            
+            setcookie('currency', $this->session->data['currency'], $option);
+            
+            // Return updated currency info
+            $json['success'] = true;
+            $json['currency'] = [
+                'code'         => $currency_info['code'],
+                'title'        => $currency_info['title'],
+                'symbol_left'  => $currency_info['symbol_left'],
+                'symbol_right' => $currency_info['symbol_right']
+            ];
+        }
+        
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($json));
     }
